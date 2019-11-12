1e2aa20eaa534aa4fe20abae15efd265fce0b6bb
joek47 lap <joe.koh47@yahoo.com>
2019-11-12 17:59:14 +0800

    ex1

---
+++ ex1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* comment */
+int main(int argc, char *argv[]){
+
+    int distance = 100;
+
+    // comment
+    printf("You are %d km away.\n", distance);
+
+    return 0;
+}

27e6c804ada0612904c0d30f7fb0e01baf3e31f6
joek47 lap <joe.koh47@yahoo.com>
2019-11-12 18:10:12 +0800

    bad_session

--- ex3.c
+++ ex3.c
@@ -3,9 +3,15 @@
 int main(){
 
     int age = 72;
+    // int age; 
+    // I am 0 years old.
+
     int height = 100;
 
     printf("I am %d years old.\n", age);
+    // printf("I am %d years old.\n"); 
+    // I am 2061957272 years old.
+
     printf("I am %d cm tall.\n", height);
 
     return 0;

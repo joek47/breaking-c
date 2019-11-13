74a39495360cc9e56ae5fb2832e8542de6e48911
joek47 lap <joe.koh47@yahoo.com>
2019-11-13 17:11:26 +0800

    ex8

--- Makefile
+++ Makefile
@@ -4,5 +4,6 @@ all:
 	cc ex1.c -o ex1 
 	cc ex3.c -o ex3 
 	cc ex8.c -o ex8
+	cc ex9.c -o ex9
 clean:
-	rm -f ex1 ex3 ex8
+	rm -f ex1 ex3 ex8 ex9
--- ex8.c
+++ ex8.c
@@ -5,12 +5,14 @@ int main(int argc, char *argv[]){
     int i = 0;
 
     // programme name itself is one argument
-    if (argc == 1){
+    if (argc < 2){
+        printf("Please enter some arguments.\n");
+    } else if (argc == 2){
         printf("You only have one argument.\n");
-    } else if (argc > 1 && argc < 4){
+    } else if (argc > 2 && argc < 4){
         printf("Here's your arguments: \n");
 
-        for(i = 0; i < argc; i++){
+        for(i = 1; i < argc; i++){
             printf("%s ", argv[i]);
         }
         printf("\n");

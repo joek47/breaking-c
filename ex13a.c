60c4b6dcfde383d97395ecfbd4bd1c44ef13da6d
joek47 lap <joe.koh47@yahoo.com>
2019-11-14 17:04:30 +0800

    array and pointer

---
+++ ex13a.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+    char char_array[] = "abc";  // stores string
+    char *char_pointer = "abc"; //stores address to string
+
+    printf("Char array \"%s\" and Char pointer \"%s\"\n", char_array, char_pointer);
+}
+

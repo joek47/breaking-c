2aeef4c774089d5c3017a1d62219c0c7ed48ddab
joek47 <joe.koh47@yahoo.com>
2019-11-26 23:42:54 +0800

    stdint variables

--- ex21.c
+++ ex21.c
@@ -3,6 +3,8 @@
 
 int main(int argc, char *argv[])
 {
+    printf("=====\n");
+    printf("Type Sizes:\n");
     printf("int8_t size:%ld \tmax:%d \tmin:%d\n", sizeof(int8_t), INT8_MAX, INT8_MIN);
     printf("uint8_t size:%lu \tmax:%d \tmin:%d\n", sizeof(uint8_t), UINT8_MAX, 0);
 
@@ -16,8 +18,25 @@ int main(int argc, char *argv[])
     printf("uint64_t size:%ld \tmax:%lu \tmin:%d\n", sizeof(uint64_t), UINT64_MAX, 0);
     printf("\twhich is the same as UINTMAX_MAX:%lu \tor SIZE_MAX:%lu\n", UINTMAX_MAX, SIZE_MAX);
 
-    printf("biggest type possible\n\tintmax_t:%lu \tuintmax_t:%lu\n",\
+    printf("Signed integer large enough to hold a pointer:%lu\n",sizeof(intptr_t));
+    printf("Max value of a intptr_t:%ld\n\tmin value:%ld\n",INTPTR_MAX, INTPTR_MIN);
+    printf("Unsigned integer large enough to hold a pointer:%lu\n",sizeof(uintptr_t));
+    printf("Max value of a uintptr_t:%lu\n\t",UINTPTR_MAX);
+    printf("biggest type possible\tintmax_t:%lu \tuintmax_t:%lu\n",\
             sizeof(intmax_t), sizeof(uintmax_t));
-    // printf(int8_t);
+
+    printf("=====\n");
+    printf("Bit Operators:\n");
+    printf("1 shift left by 1 bit:%d\n", 1 << 1);
+    printf("1 shift right by 1 bit:%d\n", 1 >> 1);
+    printf("0 shift left by 1 bit:%d\n", 0 << 1);
+    printf("00 bitwise or 01:%d\n", 0|1);
+    printf("10 bitwise xor 01:%d\n", 2^1);
+
+    printf("=====\n");
+    printf("Type Conversion:\n");
+    printf("long double:%d \tdouble:%d \tfloat:%d \tint:%d \tlong:%d\n",\
+            sizeof(long double), sizeof(double), sizeof(float),\
+            sizeof(int), sizeof(long));
     return 0;
 }
